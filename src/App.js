3a805c920089f7e0057869879f06cc7006f0dca3
WebdevSearex <webdev@searex.co.za>
2022-11-30 16:55:27 +0200

    Continue with tutorial (2022-11-30)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
 import './App.css';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
-class App extends Component{   
-  render() {    
+// App Level Context/State
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
-    return (
-      <Router>
-          <div className='App'>
-            <Navbar></Navbar>
-            <div className='container'>            
-            <Routes>              
-                <Route path='/' element={<Home  />} />
-                <Route path="/about" element={<About />}>
-              </Route>
-            </Routes>
-              
-            </div>        
-        </div>
-      </Router>
-    );
-  }  
+const App = () => {  
+  return (
+      <GithubState>
+        <AlertState> 
+          <Router>
+              <div className='App'>
+                <Navbar></Navbar>          
+                  <div className='container'>        
+                  <Alert />
+                    <Routes>              
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/user/:login" element={<User /> } />                
+                        <Route path="*" element={<NotFound /> } />                
+                    </Routes>              
+                  </div>        
+              </div>
+          </Router>
+        </AlertState>
+      </GithubState>
+    );   
 }
 
 export default App;
